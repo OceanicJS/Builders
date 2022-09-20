141910c54e56803a2fecd6ee366cde95a69ec79c
Donovan Daniels <hewwo@yiff.rocks>
2022-09-20 12:32:53 -0500

    woo

--- esm.mjs
+++ esm.mjs
@@ -1,7 +1,7 @@
 const ActionRow = (await import("./dist/Structures/ActionRow.js")).default.default;
 const Button = (await import("./dist/Structures/Button.js")).default.default;
 const Component = (await import("./dist/Structures/Component.js")).default.default;
-const Constants = (await import("./dist/dist/Constants.js")).default;
+const Constants = (await import("./dist/Constants.js")).default;
 const SelectMenu = (await import("./dist/Structures/SelectMenu.js")).default.default;
 const TextInput = (await import("./dist/Structures/TextInput.js")).default.default;
 const ComponentBuilder = (await import("./dist/ComponentBuilder.js")).default.default;
